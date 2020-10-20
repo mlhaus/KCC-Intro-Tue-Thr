6d366a795a906bf716d4122cac51e1c225663f7f
Paula Kerezsi <paula-kerezsi@student.kirkwood.edu>
2020-10-20 14:44:19 -0500

    Made board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -9,6 +9,12 @@ def drawBoard(board):
     #-----------
     # 7 | 8 | 9
 
+    board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    print(" " + board[0] +  " | " + board[1] + " | " + board[2])
+    print("-----------")
+    print(" " + board[3] +  " | " + board[4] + " | " + board[5])
+    print("-----------")
+    print(" " + board[6] +  " | " + board[7] + " | " + board[8])
 
 def checkWinner(board, player):
     result = False
