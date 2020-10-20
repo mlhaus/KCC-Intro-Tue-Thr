48e6ae7d48eebbf48c3e1e5cd289010eb8b5a2ea
Jeremy Alan Gunderson <jagunderson.jg@gmail.com>
2020-10-20 14:41:30 -0500

    completed drawBoard function

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,7 +1,11 @@
 from helpers import getNum
 
 def drawBoard(board):
-    print()
+    print('', board[0], '|', board[1], '|', board[2])
+    print('-----------')
+    print('', board[3], '|', board[4], '|', board[5])
+    print('-----------')
+    print('', board[6], '|', board[7], '|', board[8])
     #TODO - Print the values in the board array parameter. Format them in a tic-tac-toe board
     # 1 | 2 | 3
     #-----------
