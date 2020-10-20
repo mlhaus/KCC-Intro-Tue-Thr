2ca0224d6d83e1103060b1d38ec9c16d74e17ee0
aprosesoul <joss-ridgway@student.kirkwood.edu>
2020-10-20 14:41:02 -0500

    Added Board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,11 +3,11 @@ from helpers import getNum
 def drawBoard(board):
     print()
     #TODO - Print the values in the board array parameter. Format them in a tic-tac-toe board
-    # 1 | 2 | 3
-    #-----------
-    # 4 | 5 | 6
-    #-----------
-    # 7 | 8 | 9
+    print (board[0] + "|" + board[1] + "|" board[2])
+    print ("-----------")
+    print (board[3] + "|" + board[4] + "|" board[5])
+    print ("-----------")
+    print (board[6] + "|" + board[7] + "|" board[8])
 
 
 def checkWinner(board, player):
