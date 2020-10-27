7bead0e606839efb06b1f99ee34aa724117d0004
Tresha Sarabia <treshasar@gmail.com>
2020-10-27 01:41:53 -0500

    Finished exercise.6

--- sarabia-exercise6.py
+++ sarabia-exercise6.py
@@ -1,43 +1,30 @@
 from helpers import getNum
 from random import randint
 
-#Needrandint and getNum
 
+def randomNumbers(n):
+  result = []
+  for i in range(n):
+    result.append(randint(0.0, 100.0))
+  return result
 
-# STEP 3: Define a randomNumbers function that takes a number as a parameter.
-	# STEP 4a: Create an empty array called result
-	# STEP 5: Create a loop that runs n times
-		# STEP 6: Generate a random number between 0 and 100. Add the number to the result array.
-	# STEP 4b: Return the result array variable
-
-# STEP 9: Define a largerThan function that takes an array of numbers and a single number as parameters.
-def largerThan(arr(num, single(n)))
-    # STEP 10: Create an empty array called result
+def largerThan(list, n):
     result = []
-    # STEP 11: Create a loop that runs once for each item in the list/array parameter
-    for num in array
-    	# STEP 12: Write an if statement to test if the current value in the list is greater than the number parameter
-    		# STEP 13: If true, write another if statement to test if the current value in the list is not already in the result array
-    			# STEP 14: If true, add the current value in the list to the result array
-    # STEP 15: Print the list array variable. Label it "List of numbers".
-    # STEP 16: Sort the result array variable and print it. Label it "Numbers that are greater than [n]"
+    for item in list:
+      count += 1
+      if item > n:
+        if item not in result:
+          (result.append(list)
+    print("List of numbers " + str(list)
+    result.sort
+    print("Numbers that are greater than " + str(n))
 
 
 def main():
-  # STEP 1: Prompt the user for a number between 10 and 20. Assign the user's response to a variable called howMany.
 
-  howMany = ""
-
-  # STEP 2: Call a randomNumbers function, passing the howMany variable as an argument. Assign the result to a variable called list.
-
+  howMany = getNum("Please enter a number between 10 and 20.", 10.0, 20.0)
   list = randomNumbers(howMany)
-
-  # STEP 7: Prompt the user for a number between 0 and 100. Assign the user's response to a variable called n.
-
-  n = ""
-
-  # STEP 8: Call a largerThan function, passing the list and n variables as arguments.
-
+  n = getNum("Please enter a number between 0 and 100.", 0.0, 100.0))
   largerThan(list, n)
   
 main()
