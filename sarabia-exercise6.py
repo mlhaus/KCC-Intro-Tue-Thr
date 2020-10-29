d109b1004d09524a31e20e6790a6346c4d4fc1f9
Tresha Sarabia <treshasar@gmail.com>
2020-10-29 16:43:49 -0500

    Fixed errors in Exercise 6

--- sarabia-exercise6.py
+++ sarabia-exercise6.py
@@ -5,7 +5,7 @@ from random import randint
 def randomNumbers(n):
   result = []
   for i in range(n):
-    result.append(randint(0.0, 100.0))
+    result.append(randint(0, 100))
   return result
 
 def largerThan(list, n):
@@ -17,14 +17,14 @@ def largerThan(list, n):
           (result.append(list))
     print("List of numbers " + str(list))
     result.sort
-    print("Numbers that are greater than " + str(n))
+    print("Numbers that are greater than [n]: " + str(result))
 
 
 def main():
 
-  howMany = getNum("Please enter a number between 10 and 20.", 10.0, 20.0)
+  howMany = getNum("Please enter a number between 10 and 20.", 10, 20)
   list = randomNumbers(howMany)
-  n = getNum("Please enter a number between 0 and 100.", 0.0, 100.0))
+  n = getNum("Please enter a number between 0 and 100.", 0, 100))
   largerThan(list, n)
   
 main()
